4f34c982ac2e91a0e4fa111b3d4678452f0503e7
taylor y <64106592+tayleyi@users.noreply.github.com>
2021-12-13 22:46:29 -0500

    [12/13] addition: started shifting towards permutation method

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -1,71 +1,34 @@
 <script>
-	export let easy;
-	let board = [];
+	let board = new Set();
+	const I_9 = [[1, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 1, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 1, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 1, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 1, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 1, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 1]
+	];
 
-	let neq_indices = [];		// these are the index values that each index cannot be equal to
+	function create_permutation(starting_matrix) {
+		let permutation = [];
+		for (let i = 0; i < starting_matrix.length; i++) {
+			let row = Math.floor(Math.random() * starting_matrix.length);
 
-	for (let i = 0; i < 81; i++) {
-		let indices = new Set();
-
-		// row limitations
-		let s = 9 - (i % 9);
-		for (let k = s - 9; k < s; k++) {
-			if (i != i + k) {
-				indices.add(i + k);
+			if (row in starting_matrix) {
+				permutation.push(starting_matrix[row]);
+				starting_matrix.splice(row, 1);
 			}
 		}
-
-		// column limitations
-		for (let k = -72; k < 81; k += 9) {
-			if (i + k >= 0 && i + k < 81 && i + k != i) {
-				indices.add(i + k);
-			}
-		}
-
-		// 3x3 limitations
-		let base = [0, 1, 2, 9, 10, 11, 18, 19, 20];
-		let found = false;
-		let m = 0;
-		for (let m = 0; m < 3; m++) {			// row
-			for (let n = 0; n < 3; n++) {		// column
-				found = base[i] + (27 * n) + (3 * m) == i;
-				if (found) {
-					for (let b of base) {
-						let r = (b + (27 * n) + (3 * m));
-						indices.add(r);
-					}
-				break;
-				}
-			}
-			if (found) {
-				break;
-			}
-		}
-		neq_indices.push(indices);
+		return permutation;
 	}
 
-	for (let i = 0; i < 81; i++) {
-		let neq = [];
-		let x_i = Math.floor(Math.random() * 9 + 1);
-		for (let value of neq_indices[i]) {
-			if (board[value] == x_i) {
-				x_i = Math.floor(Math.random() * 9 + 1);
-			}
-		}
-		board.push(x_i);
+	while (board.size < 9) {
+		board.add(create_permutation(I_9));
 	}
 
-	let upperLimit;
 	let blanks = [];
-	if (easy) {
-		upperLimit = 20;
-	}
-	else {
-		upperLimit = 40;
-	}
-	for (let k = 0; k < upperLimit; k++) {
-		blanks.push(Math.floor(Math.random() * 81 + 1));
-	}
 </script>
 
 <div>
